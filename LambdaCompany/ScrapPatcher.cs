b95c4077a07b645021279f436be82e7959090df9
Noosh <nooshi34@gmail.com>
2024-01-26 01:13:44 -0500

    audio mixer fix from lethallib

--- LambdaCompany/ScrapPatcher.cs
+++ LambdaCompany/ScrapPatcher.cs
@@ -12,12 +12,12 @@ namespace LambdaCompany
 		{
 			//Activate Patches for Scrap Items
 			On.StartOfRound.Awake += StartOfRound_Awake;
+			//audio mixer patch
+			On.StartOfRound.Start += StartOfRound_Start;
+			On.MenuManager.Start += MenuManager_Start;
 		}
 
-		public static ScrapEntry GetEntry(string item)
-		{
-			return scrapCatelog[item];
-		}
+		public static ScrapEntry GetEntry(string item) { return scrapCatelog[item]; }
 
 		private static void StartOfRound_Awake(On.StartOfRound.orig_Awake orig, StartOfRound self)
 		{
@@ -27,8 +27,7 @@ namespace LambdaCompany
 				foreach (ScrapEntry scrap in scrapCatelog.Values)
 				{
 					if (level.spawnableScrap.Any(x => x.spawnableItem == scrap.item)) continue;
-					if (scrap.levelBlacklist != null)
-					{
+					if (scrap.levelBlacklist != null) {
 						if (scrap.levelBlacklist.Contains(level.name)) continue;
 					}
 					level.spawnableScrap.Add(new SpawnableItemWithRarity() { spawnableItem = scrap.item, rarity = scrap.rarity });
@@ -40,10 +39,77 @@ namespace LambdaCompany
 				if (!self.allItemsList.itemsList.Contains(scrap.item))
 				{
 					P.Log($"Registered item: {scrap.item.itemName}");
-
 					self.allItemsList.itemsList.Add(scrap.item);
 				}
 			}
+		}
+
+		internal static List<GameObject> prefabsToFix = new List<GameObject>();
+		internal static List<GameObject> fixedPrefabs = new List<GameObject>();
+		private static void StartOfRound_Start(On.StartOfRound.orig_Start orig, StartOfRound self)
+		{
+			AudioMixer audioMixer = SoundManager.Instance.diageticMixer;
+			List<GameObject> prefabsToRemove = new List<GameObject>();
+			for (int i = prefabsToFix.Count - 1; i >= 0; i--)
+			{
+				GameObject prefab = prefabsToFix[i];
+				AudioSource[] audioSources = prefab.GetComponentsInChildren<AudioSource>();
+				foreach (AudioSource audioSource in audioSources)
+				{
+					if (audioSource.outputAudioMixerGroup == null) continue;
+					if (audioSource.outputAudioMixerGroup.audioMixer.name == "Diagetic")
+					{
+						var mixerGroup = audioMixer.FindMatchingGroups(audioSource.outputAudioMixerGroup.name)[0];
+						if (mixerGroup != null)
+						{
+							audioSource.outputAudioMixerGroup = mixerGroup;
+							P.Log($"Set mixer group for {audioSource.name} in {prefab.name} to Diagetic:{mixerGroup.name}");
+							prefabsToRemove.Add(prefab);
+						}
+					}
+				}
+			}
+			foreach (GameObject prefab in prefabsToRemove)
+			{
+				prefabsToFix.Remove(prefab);
+			}
+			orig(self);
+		}
+		private static void MenuManager_Start(On.MenuManager.orig_Start orig, MenuManager self)
+		{
+			orig(self);
+			if (self.GetComponent<AudioSource>() == null) return;
+			AudioMixer audioMixer = self.GetComponent<AudioSource>().outputAudioMixerGroup.audioMixer;
+			List<GameObject> prefabsToRemove = new List<GameObject>();
+			for (int i = prefabsToFix.Count - 1; i >= 0; i--)
+			{
+				GameObject prefab = prefabsToFix[i];
+				AudioSource[] audioSources = prefab.GetComponentsInChildren<AudioSource>();
+				foreach (AudioSource audioSource in audioSources)
+				{
+					if (audioSource.outputAudioMixerGroup == null) continue;
+					if (audioSource.outputAudioMixerGroup.audioMixer.name == "NonDiagetic")
+					{
+						var mixerGroup = audioMixer.FindMatchingGroups(audioSource.outputAudioMixerGroup.name)[0];
+						if (mixerGroup != null)
+						{
+							audioSource.outputAudioMixerGroup = mixerGroup;
+							P.Log($"Set mixer group for {audioSource.name} in {prefab.name} to NonDiagetic:{mixerGroup.name}");
+							prefabsToRemove.Add(prefab);
+						}
+					}
+				}
+			}
+			foreach (GameObject prefab in prefabsToRemove)
+			{
+				prefabsToFix.Remove(prefab);
+			}
+		}
+		public static void FixMixerGroups(GameObject prefab)
+		{
+			if (fixedPrefabs.Contains(prefab)) return;
+			fixedPrefabs.Add(prefab);
+			prefabsToFix.Add(prefab);
 		}
 	}
 
@@ -51,9 +117,10 @@ namespace LambdaCompany
 	{
 		public ScrapEntry(string assetpath, int rarity, List<string>? levels) : this()
 		{
-			item = P.assets.LoadAsset<Item>(assetpath); ;
+			item = P.assets.LoadAsset<Item>(assetpath);
 			this.rarity = rarity;
 			this.levelBlacklist = levels;
+			ScrapPatcher.FixMixerGroups(item.spawnPrefab);
 		}
 		internal Item item;
 		internal int rarity;
